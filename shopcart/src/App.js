25cbf2e22e3534f1d9a145cf37111a6299ab6f69
Aayushi132 <112529819+Aayushi132@users.noreply.github.com>
2023-01-29 21:29:59 -0800

    commit

--- shopcart/src/App.js
+++ shopcart/src/App.js
@@ -10,34 +10,7 @@ import { CartData } from "./CartProduct";
 import Nav from "./Nav";
 import "./style.css";
 
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       Products: ProductData,
-//       CartProducts: CartData,
-//     };
-//   }
 
-//   render() {
-//     return (
-//       {},
-//       (
-//         <div className="App text-secondary">
-//           <Nav
-//             products={this.state.Products.Products}
-//             cartTotalProduct={this.state.Products.TotalCartItems}
-//             cartItems={this.state.CartProducts.CartProducts}
-//           />
-          
-//           {/* pass data as attributes */}
-//         </div>
-//       )
-//     );
-//   }
-// }
-
-// export default App;
 
 const App  = () =>{
   const [Products, setProducts] = useState(ProductData)
